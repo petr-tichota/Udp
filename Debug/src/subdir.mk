8c9729067c4072129e2f262a12060e362fce58a6
petr-tichota <z003znje@md1y35fc.ad001.siemens.net>
2018-10-25 13:30:10 +0200

    asdasd

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/udpClient.c \
+../src/udpServer.c 
+
+OBJS += \
+./src/udpClient.o \
+./src/udpServer.o 
+
+C_DEPS += \
+./src/udpClient.d \
+./src/udpServer.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
